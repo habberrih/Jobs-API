c48a360260c95de97b1e2958ab243cc252121df3
12a02d <abdul2013ly@gmail.com>
2022-10-20 11:41:55 +0200

    added security packages

--- server/src/app.js
+++ server/src/app.js
@@ -4,6 +4,12 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// Security requiring modules
+const helmet = require('helmet');
+const cors = require('cors');
+const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
+
 const authRouter = require('./routes/auth');
 const jobsRouter = require('./routes/jobs');
 const authenticateUser = require('./middleware/authentication');
@@ -11,7 +17,19 @@ const authenticateUser = require('./middleware/authentication');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 const notFoundMiddleware = require('./middleware/not-found');
 
+const limiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
+
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // Routes
 app.use('/api/v1/auth', authRouter);
